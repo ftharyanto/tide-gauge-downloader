81326112cf533f0d3748815265621a2f01108918
Fajar Tri Haryanto <ftfajar5@gmail.com>
2021-10-02 12:22:10 +0700

    remove unused js library
    removed jquery and selectpicker, moved js file to the bottom of the body

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -10,23 +10,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.css">
 
-    <!-- jquery -->
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-
     <!-- custom script and styling -->
-    <script src=<?php echo base_url($js); ?>></script>
     <link rel="stylesheet" href=<?php echo base_url($css); ?>>
     <title><?= $title; ?></title>
+
 </head>
 
 <body>
 
     <?= $this->renderSection('content'); ?>
 
-    <!-- bootstrap, bootstrap select, jquery js -->
+    <!-- custom script -->
+    <script src=<?php echo base_url($js); ?>></script>
+
+    <!-- bootstrap, bootstrap select -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js"></script>
 </body>
 
 </html>
