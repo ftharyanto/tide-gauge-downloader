5a80cf70b7b5c34347b6d634e549435c55e7fa79
Fajar Tri Haryanto <ftfajar5@gmail.com>
2021-10-18 10:23:09 +0700

    add leaflet js

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -14,6 +14,10 @@
     <link rel="stylesheet" href=<?php echo base_url($css); ?>>
     <title><?= $title; ?></title>
 
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
 </head>
 
 <body>
@@ -25,6 +29,9 @@
 
     <!-- bootstrap, bootstrap select -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- leaflet map -->
+    <script src="<?php echo base_url($map); ?>"></script>
 </body>
 
 </html>
